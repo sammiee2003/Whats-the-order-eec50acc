8a97ce1399c70d4539beff6457e023c5178b27f9
SAM VAN DEN BOSCH <samvandenbosch2003@gmail.com>
2020-04-22 11:43:27 +0200

    Uitwerking van de exercise

--- queries.sql
+++ queries.sql
@@ -2,4 +2,4 @@ use netland;
 
 -- select*from series where rating >2.5 order by rating desc;
 -- select * from series where seasons <5 order by seasons asc;
-select seasons, country from series where seasons <3 or seasons >20 order by seasons, country desc;
+-- select seasons, country from series where seasons <3 or seasons >20 order by seasons, country desc;
