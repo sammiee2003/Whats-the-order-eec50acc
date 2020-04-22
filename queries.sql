c1f4109199fdd325a655e793dcfb29df51b9759d
SAM VAN DEN BOSCH <samvandenbosch2003@gmail.com>
2020-04-22 11:43:09 +0200

    Uitwerking van de exercise

--- queries.sql
+++ queries.sql
@@ -1,10 +1,5 @@
 use netland;
 
--- select title from series where has_won_awards = 1;
--- select title from series where rating > 2.5;
--- select title from series where language and country = 'NL';
--- select title from series where seasons <5;
--- select title from series order by rating desc limit 1;
--- select title from series where seasons <3 or seasons > 20;
--- select * from series where title like 'th%';
--- select title from series where seasons <3 or seasons >3;
+-- select*from series where rating >2.5 order by rating desc;
+-- select * from series where seasons <5 order by seasons asc;
+select seasons, country from series where seasons <3 or seasons >20 order by seasons, country desc;
